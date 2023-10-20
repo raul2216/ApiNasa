df2728a2254bafa9cc1c6e8e617ebb7761530336
raul2216 <raulpcq22@gmail.com>
2023-10-20 01:21:59 -0500

    modificaciones en el archivo App.js

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header/Header';
 import Home from './src/Views/Home/Home';
 import { SafeAreaView } from 'react-native';
-
+import { Provider } from 'react-redux';
+import {store} from "./redux/store"
 export default function App() {
   return (
-
-    <SafeAreaView>
+  <Provider store={store}>
+    <SafeAreaView  style={[styles.container, {paddingTop: Platform.OS === "android" && 30}]}>
         <Home/>
       <StatusBar style="auto" />
     </SafeAreaView>
+  </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'rgba(7, 26, 93, 255)',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

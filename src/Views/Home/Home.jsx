6ab952db56c8b3e99d2a4a1c7e782da69ee7855f
raul2216 <raulpcq22@gmail.com>
2023-10-20 01:20:54 -0500

    hice algunas modificaciones en los componentes home y header

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,14 +1,35 @@
+import { View, Text, StyleSheet } from "react-native";
+import Header from "../../components/Header/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getNasa } from "../../../redux/actions";
+export default function Home() {
+  //   const Nasa = useSelector((state) => state.nasa)
 
-import { View, Text, StyleSheet } from "react-native"
-import Header from "../../components/Header/Header"
-export default function Home () {
 
-    return (
-        <View>
-            <View>
-                <Header/>
-            </View>
-        </View>
-    )
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const loadTodayImage = async () => {
+      try {
+        const todayImage = await dispatch(getNasa());
+        console.log(todayImage);
+      } catch (error) {
+        console.error(error)
+      }
+    };
 
+    loadTodayImage()
+  }, [getNasa]);
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+});
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,46 @@
 import React from "react";
 
-import { View, Text, StyleSheet, } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import { Image } from "react-native";
 
-const img = require("../../../assets/Nasa-logo.png")
-console.log(img)
+const img = require("../../../assets/Nasa-logo.png");
 export default function Header() {
   return (
-    <View>
-      <View>
-        <Text>Explore</Text>
+    <View style={styles.container}>
+      <View styles={styles.leftContainer}>
+        <Text style={styles.title}>Explore</Text>
       </View>
 
-      <View>
-       <Image  source={require('../../../assets/Nasa-logo.png')}/>
-
+      <View style={styles.rightContainer}>
+        <Image
+          source={require("../../../assets/Nasa-logo.png")}
+          style={styles.image}
+        />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  leftContainer: {
+    flex: 1,
+    alignItems: "flex-start",
+  },
+  rightContainer: {
+    flex: 1,
+    alignItems: "flex-end",
+  },
+  title: {
+    fontSize: 29,
+    color: "#ffffff",
+  },
+  image: {
+    width: 60,
+    height: 60,
+  },
+});

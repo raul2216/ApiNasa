b825be7979363d2f712e157ac5128e80b9c182fa
raul2216 <raulpcq22@gmail.com>
2023-10-22 23:01:41 -0500

    redux

--- redux/actions.js
+++ redux/actions.js
@@ -9,7 +9,6 @@ export function getNasa() {
     return async function (dispatch) {
         try {
             const nasa = (await axios.get(`https://api.nasa.gov/planetary/apod?api_key=8202nANlsnYqqKma0AyZTX52OSpoRxAdGlzUc19a`)).data
-            console.log(nasa,)
             dispatch({ type: GET_NASA, payload: nasa })
 
         } catch (error) {
